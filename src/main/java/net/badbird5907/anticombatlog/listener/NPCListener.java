fe9d75080daadbd06d2ee61f1ddb38d8a4201ce9
Badbird5907 <50347938+Badbird5907@users.noreply.github.com>
2023-01-25 14:53:37 -0500

    add a null check

--- src/main/java/net/badbird5907/anticombatlog/listener/NPCListener.java
+++ src/main/java/net/badbird5907/anticombatlog/listener/NPCListener.java
@@ -4,6 +4,7 @@ import net.advancedplugins.ae.api.AEAPI;
 import net.badbird5907.anticombatlog.AntiCombatLog;
 import net.badbird5907.anticombatlog.object.CombatNPCTrait;
 import net.citizensnpcs.api.event.NPCDeathEvent;
+import net.citizensnpcs.api.npc.NPC;
 import org.bukkit.Bukkit;
 import org.bukkit.GameRule;
 import org.bukkit.event.EventHandler;
@@ -14,22 +15,25 @@ import java.util.stream.Collectors;
 public class NPCListener implements Listener {
     @EventHandler
     public void onNpcDeath(NPCDeathEvent event) {
-        event.setDroppedExp((int) event.getNPC().getTrait(CombatNPCTrait.class).getXp());
-        Boolean value = event.getEvent().getEntity().getWorld().getGameRuleValue(GameRule.KEEP_INVENTORY);
-        if (Boolean.FALSE.equals(value)) {
-            if (Bukkit.getPluginManager().isPluginEnabled("AdvancedEnchantments")) {
-                event.getDrops().addAll(event.getNPC().getTrait(CombatNPCTrait.class).getItems().stream()
-                        .filter(item -> !AEAPI.hasWhitescroll(item)).collect(Collectors.toList()));
-            } else {
-                event.getDrops().addAll(event.getNPC().getTrait(CombatNPCTrait.class).getItems());
+        NPC npc = event.getNPC();
+        if (npc != null) {
+            event.setDroppedExp((int) npc.getTrait(CombatNPCTrait.class).getXp());
+            Boolean value = event.getEvent().getEntity().getWorld().getGameRuleValue(GameRule.KEEP_INVENTORY);
+            if (Boolean.FALSE.equals(value)) {
+                if (Bukkit.getPluginManager().isPluginEnabled("AdvancedEnchantments")) {
+                    event.getDrops().addAll(npc.getTrait(CombatNPCTrait.class).getItems().stream()
+                            .filter(item -> !AEAPI.hasWhitescroll(item)).collect(Collectors.toList()));
+                } else {
+                    event.getDrops().addAll(npc.getTrait(CombatNPCTrait.class).getItems());
+                }
             }
-        }
-        //event.getDrops().addAll(Arrays.stream(event.getNPC().getTrait(Equipment.class).getEquipment()).toList());
-        String killer;
-        if (event.getEvent().getEntity().getKiller() == null)
-            killer = "null"; //https://discord.com/channels/315163488085475337/315625512753954816/916351371970740226 on citizens support discord
-        else killer = event.getEvent().getEntity().getKiller().getName();
-        AntiCombatLog.getToKillOnLogin().put(event.getNPC().getTrait(CombatNPCTrait.class).getUuid(), killer);
-        AntiCombatLog.saveData();
+            //event.getDrops().addAll(Arrays.stream(npc.getTrait(Equipment.class).getEquipment()).toList());
+            String killer;
+            if (event.getEvent().getEntity().getKiller() == null)
+                killer = "null"; //https://discord.com/channels/315163488085475337/315625512753954816/916351371970740226 on citizens support discord
+            else killer = event.getEvent().getEntity().getKiller().getName();
+            AntiCombatLog.getToKillOnLogin().put(npc.getTrait(CombatNPCTrait.class).getUuid(), killer);
+            AntiCombatLog.saveData();
+        } else Bukkit.getLogger().warning("NPC was null in NPCDeathEvent!");
     }
 }
