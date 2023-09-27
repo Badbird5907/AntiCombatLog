c32dd410417ba4944ee06fc414e6c87dc2865628
Badbird5907 <50347938+Badbird5907@users.noreply.github.com>
2023-09-27 09:01:06 -0400

    fix startup without ExcellentEnchants

--- src/main/java/net/badbird5907/anticombatlog/listener/NPCListener.java
+++ src/main/java/net/badbird5907/anticombatlog/listener/NPCListener.java
@@ -2,10 +2,7 @@ package net.badbird5907.anticombatlog.listener;
 
 import net.advancedplugins.ae.api.AEAPI;
 import net.badbird5907.anticombatlog.AntiCombatLog;
-import net.badbird5907.anticombatlog.api.events.CombatLogKillEvent;
-import net.badbird5907.anticombatlog.api.events.CombatLogNPCSpawnEvent;
-import net.badbird5907.anticombatlog.api.events.CombatTagEvent;
-import net.badbird5907.anticombatlog.api.events.UnCombatTagEvent;
+import net.badbird5907.anticombatlog.hooks.impl.ExcellentEnchantUtil;
 import net.badbird5907.anticombatlog.object.CombatNPCTrait;
 import net.badbird5907.blib.util.Logger;
 import net.citizensnpcs.api.event.NPCDeathEvent;
@@ -58,29 +55,7 @@ public class NPCListener implements Listener {
             return AEAPI.hasWhitescroll(item);
         } else if (Bukkit.getPluginManager().isPluginEnabled("ExcellentEnchants")) {
             Logger.debug("ExcellentEnchants is enabled!");
-            Map<ExcellentEnchant, Integer> excellents = EnchantUtils.getExcellents(item);
-            if (excellents.isEmpty()) return false;
-            Logger.debug("Found " + excellents.size() + " excellent enchants on " + item.getType().name() + "!");
-            return excellents.keySet().stream().anyMatch(enchant -> {
-                Logger.debug(" - Checking if " + enchant.getId() + " is a soulbound enchant...");
-                // return true to add to list
-                if (enchant == null) return false;
-                if (enchant.getId().equalsIgnoreCase("soulbound")) {
-                    Logger.debug("  - Found soulbound enchant!");
-                    if (EnchantUtils.isOutOfCharges(item, enchant)) {
-                        Logger.debug("  - Out of charges!");
-                        return false;
-                    }
-                    if (EnchantUtils.getLevel(item, enchant) < 0) {
-                        Logger.debug("  - Level is less than 0!");
-                        return false;
-                    }
-                    // Logger.debug("  - Consuming charges...");
-                    // EnchantUtils.consumeCharges(item, enchant, EnchantUtils.getLevel(item, enchant)); // disabled because excellent enchants does this for us when the player is kiled
-                    return true;
-                }
-                return false;
-            });
+            return ExcellentEnchantUtil.shouldKeep(item);
         } else {
             return false;
         }
